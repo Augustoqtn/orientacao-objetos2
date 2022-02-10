2b77836f84dd90fe4e8a172174e8cecf81c426be
Augusto <augustinhobrito542@gmail.com>
2022-02-10 13:23:35 -0300

    usa o parent(que chama a classe pai) e tambem usa o protected(que permite que a classe atual e as classes filhas tenham acesso).

--- src/Funcionario.php
+++ src/Funcionario.php
@@ -6,15 +6,19 @@ class Funcionario extends Pessoa
 
     public function __construct($nome,CPF $cpf,$cargo)
     {
-        $this->validaNomeTitular($nome);
-        $this->nome = $nome;
-        $this->cpf = $cpf;
+        parent::__construct($nome,$cpf);
         $this->cargo = $cargo;
     }
 
     public function recuperaCargo()
     {
         return $this->cargo;
+    }
+
+    public function alteraNome($nome):void
+    {
+        $this->validaNomeTitular($nome);
+        $this->nome = $nome;
     }
 
 }
