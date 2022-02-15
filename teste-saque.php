8542594c2568a49c8c672e20e1eba96ada8de3c3
Augusto <augustinhobrito542@gmail.com>
2022-02-14 22:24:35 -0300

    nao esta funcionando quando saca valor da conta-corrente

--- teste-saque.php
+++ teste-saque.php
@@ -1,14 +1,14 @@
 <?php
-
-use Alura\Banco\Modelo\Conta\Conta;
 use Alura\Banco\Modelo\Conta\ContaPoupanca;
+use Alura\Banco\Modelo\Conta\Conta;
+use Alura\Banco\Modelo\conta\ContaCorrente;
 use Alura\Banco\Modelo\Conta\Titular;
 use Alura\Banco\Modelo\CPF;
 use Alura\Banco\Modelo\Endereco;
 
 require_once 'autoload.php';
 
-$conta = new ContaPoupanca(
+$conta = new ContaCorrente(
     new Titular(
         new CPF('123.456.789-10'),
         'Augusto Quintana',
@@ -16,7 +16,7 @@ $conta = new ContaPoupanca(
 
             )
     );
-
+    
     $conta->deposita(500);
     $conta->saca(100);
     echo $conta->recuperaSaldo();
