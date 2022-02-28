7e77b4175e8ee1344cd529a22957c429a2ee924c
Augusto <augustinhobrito542@gmail.com>
2022-02-28 12:37:49 -0300

    converte objeto em string

---
+++ enderecos.php
@@ -0,0 +1,11 @@
+<?php
+
+use Alura\Banco\Modelo\Endereco;
+
+require_once 'autoload.php';
+
+$umEndereco = new Endereco('Novo Hamburgo', 'canudos', 'potiguara', '1310');
+$segundoEndereco = new Endereco('Sapiranga', 'Centro', 'bonifacio', '45B');
+
+echo $umEndereco . PHP_EOL;
+echo $segundoEndereco;
